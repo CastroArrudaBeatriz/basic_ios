d29dfbc9d6fb908f797759d5506f30992933a567
beatriz.castro <beatriz_arruda_castro@hotmail.com>
2021-03-04 23:31:38 -0300

    init commit, navigation with tabBarController

---
+++ pocTabBarController/pocTabBarController/MyTabBarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MyTabBarViewController.swift
+//  pocTabBarController
+//
+//  Created by aluno on 04/03/21.
+//  Copyright © 2021 com.bac. All rights reserved.
+//
+
+import UIKit
+
+class MyTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.selectedIndex = 1
+    }
+    
+
+
+}
---
+++ pocTabBarController/pocTabBarController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pocTabBarController
+//
+//  Created by aluno on 04/03/21.
+//  Copyright © 2021 com.bac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
